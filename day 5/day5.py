3d7e3bb78d5bde70aa423bdfe871a736546e4b90
wafel <thewafelek@gmail.com>
2024-12-05 12:17:52 +0100

    code review by llm day 5 v1

--- day 5/day5.py
+++ day 5/day5.py
@@ -1,43 +1,36 @@
-rules = {}
+from collections import defaultdict
+
+rules = defaultdict(list)
 updates = []
 with open("input", "r") as file:
     for line in file:
         if "|" in line:
-            (page_bofore, page_after) = line.split("|")
-
-            # add page_before to rules
-            page_before = int(page_bofore.strip())
-            page_after = int(page_after.strip())
-            if page_before not in rules:
-                rules[page_before] = [page_after]
-            else:
-                rules[page_before].append(page_after)
+            (page_before, page_after) = map(int, line.split("|"))
+            rules[page_before].append(page_after)
 
         elif "," in line:
-            updates.append([int(l) for l in line.strip().split(",")])
+            updates.append(list(map(int, line.strip().split(","))))
 
 print(rules)
 print(updates)
 
 correct_updates = []
 for update in updates:
-    not_correct = False
+    not_valid = False
     for entry_idx in range(len(update) - 1, -1, -1):
-        # print(update[entry_idx])
         if update[entry_idx] in rules:
-            entries_to_check = update[0:entry_idx]
-            for entry in rules[update[entry_idx]]:
-                if entry in entries_to_check:
-                    not_correct = True
-                    break
+            entries_to_check = set(update[0:entry_idx])
+            if any(entry in entries_to_check for entry in rules[update[entry_idx]]):
+                not_valid = True
+                break
 
-    if not not_correct:
+    if not not_valid:
         correct_updates.append(update)
 
 print(correct_updates)
 
-summm = 0
+total_sum = 0
 for update in correct_updates:
-    summm += update[len(update)//2]
+    total_sum += update[len(update) // 2]
 
-print(summm)
+print(total_sum)
